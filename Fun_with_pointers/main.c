f8891d630e3d3086f4683925697245f17adc07f2
tsims115 <tsims115@yahoo.com>
2022-06-09 08:18:02 -0700

    Chaning live coding to this

--- Fun_with_pointers/main.c
+++ Fun_with_pointers/main.c
@@ -4,48 +4,49 @@
 
 int main()
 {
-	int array[] = {0, 1, 2};
-	int *array1 = array;
-	int *array2 = malloc(3);
-	int **array_of_arrays = malloc(3 * 3);
 	int i, j;
+	int **array_of_arrays = malloc(3 * 3);
+	int array[3] = {0, 1, 2};
+	int *array1 = malloc(3);
+	int *array2 = malloc(3);
 
-	printf("Array address: %p\n", &array);
-	printf("Pointer value: %p\n", array1);
-	printf("Pointer Address: %p\n", &array1);
+	array1[0] = 3;
+        array1[1] = 4;
+        array1[2] = 5;
+	array2[0] = 6;
+	array2[1] = 7;
+	array2[2] = 8;
 
-	array1 = array2;
-	array2[0] = 2;
-	array2[1] = 3;
-	array2[2] = 4;
-	printf("array1 first element: %d\n", array1[0]);
+	printf("array address: %p\n", &array);
+	printf("Pointer Value: %p\n", array1);
+	printf("dereferance pointer: %d\n", *array1);
+
+	for(i = 0; i < 3; i++)
+	{
+		printf("%d ", array1[i]);
+	}
+	printf("\n");
+	for(i = 0; i < 3; i++)
+        {
+                printf("%d ", *(array1 + i));
+        }
+        printf("\n");
 	for (i = 0; i < 3; i++)
 	{
-		printf("%d ", array2[i]);
+		printf("%d ",  *array1);
+		array1++;
 	}
 	printf("\n");
-	for(i = 0; i < 3; i++)
-	{
-		printf("%d ", *(array2 + i));
-	}
-	printf("\n");
-	printf("%d ", *array2);
-	array2++;
-	printf("%d ", *array2);
-	array2--;
-	printf("%d\n", *array2);
-	array1 = malloc(3);
-	array1[0] = 5;
-	array1[1] = 6;
-	array1[2] = 7;
-
+	printf("array1 now points to last element: %d\n", *array1);
+	array1 = array1 - 3;
+	printf("array1 now points to back to first element: %d\n", *array1);
 	array_of_arrays[0] = array;
-	array_of_arrays[1] = array2;
-	array_of_arrays[2] = array1;
-
-	 printf("Array of arrays\n");
+	array_of_arrays[1] = array1;
+	array_of_arrays[2] = array2;
 	for(i = 0; i < 3; i++)
 		for(j = 0; j < 3; j++)
-			printf("%d ", array_of_arrays[i][j]);
+		{
+			printf("%d, ", array_of_arrays[i][j]);
+		}
 	printf("\n");
 }
